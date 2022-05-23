4e3f23fd0466d6bc4d709bfedc9b5e13b7d69215
Lucia Morena <luciamorena77@gmail.com>
2022-05-23 17:51:17 -0300

    commit 1

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('express')()
 const port = process.env.PORT || 3000
 
 app.get('/',(req,res)=>{
-    res.status(200).send(`API 1.0.0 - ${Math.random()}`)
+    res.status(200).send(`API 2.0.0 - ${Math.random()}`)
 })
 
 app.listen(port,() => console.log(`SERVIDOR ONLINE  - ${port}`))
