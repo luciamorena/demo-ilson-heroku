cd1158743da673e80571cbd49130d8509de015d2
Lucia Morena <luciamorena77@gmail.com>
2022-05-23 10:24:23 -0300

    commit inicial

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv/config')
+
+const app = require('express')()
+const port = process.env.port || 3000
+
+app.get('/',(req,res)=>{
+    res.status(200).send(`API 1.0.0 - ${Math.random()}`)
+})
+
+app.listen(port,() => console.log(`SERVIDOR ONLINE - ${port}`))
